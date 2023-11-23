3b55e21f98a44e8de6ffbfbbe52c9fcd3412b648
RusikVolc <ruclanvolchetskyy1990@gmail.com>
2023-11-23 17:15:39 +0200

    Change text_answer field type to CharField, commented profiles listing

--- Backend/quiz_craft/urls.py
+++ Backend/quiz_craft/urls.py
@@ -6,7 +6,7 @@ from .views.task_views import TaskCreateView, TaskListView, TaskDetailView, Task
 from .views.user_views import UserProfileListView, UserRegistrationView, UserLoginView, UserLogoutView
 
 urlpatterns = [
-    path('userprofiles/', UserProfileListView.as_view(), name='userprofile-list'),
+    # path('userprofiles/', UserProfileListView.as_view(), name='userprofile-list'),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
